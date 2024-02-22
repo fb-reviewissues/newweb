cf53a135643bddd92274d4bff9628e9c578d61e9
fb-reviewissues <160053446+fb-reviewissues@users.noreply.github.com>
2024-02-22 11:14:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ form.addEventListener("submit", function (e) {
   // Use XMLHttpRequest or fetch API for AJAX request
   var xhr = new XMLHttpRequest();
 
-  xhr.open("POST", "sendMail.php", true);
+  xhr.open("POST", "https://usebasin.com/f/e1172e753e22", true);
   xhr.onreadystatechange = function () {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var data = JSON.parse(xhr.responseText);
